da645d179786e078aada4323fa8d300b50e5bad4
Andrés Muñoz <andresestebanmu@gmail.com>
2024-02-01 17:43:26 -0300

    cambios conexion

--- src/components/GaleriaCategorias.jsx
+++ src/components/GaleriaCategorias.jsx
@@ -19,45 +19,45 @@ export const GaleriaCategorias = () => {
 
       <Link to="/Gin" className="image-gallery-with-text-overlay" >
       <div className="text-with-text-overlay">GIN</div>
-        <img src="/src/assets/fotos-galeria-categorias/gin.avif" className="box" alt="" />
+        <img src="https://images.unsplash.com/photo-1542895324-076f3fb6c2cb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="box" alt="" />
       </Link>
 
       <Link to="/Pisco" className="image-gallery-with-text-overlay" >
       <div className="text-with-text-overlay">PISCO</div>
-        <img src="../assets/fotos-galeria-categorias/pisco.png" className="box" alt="" />
+        <img src="https://www.kitchencenter.cl/cdn/shop/products/Mistral_nobel_dorado_AMB_WEB0_700x.jpg?v=1666284282" className="box" alt="" />
       </Link>
       <Link to="/Ron" className="image-gallery-with-text-overlay" >
       <div className="text-with-text-overlay">RON</div>
-        <img src="/src/assets/fotos-galeria-categorias/ron.avif" className="box" alt="" />
+        <img src="https://http2.mlstatic.com/D_NQ_NP_837293-MLU71678657160_092023-O.webp" className="box" alt="" />
       </Link>
       <Link to="/Tequila" className="image-gallery-with-text-overlay" >
       <div className="text-with-text-overlay">TEQUILA</div>
-        <img src="/src/assets/fotos-galeria-categorias/tequila.avif" className="box" alt="" />
+        <img src="https://wallpapers.com/images/high/jose-cuervo-tequila-bottle-with-lemon-m47j8bgvxsusz6vn.webp" className="box" alt="" />
       </Link>
       <Link to="/Vinos" className="image-gallery-with-text-overlay">
       <div className="text-with-text-overlay">VINO</div>
-        <img src="/src/assets/fotos-galeria-categorias/vino.avif" className="box" alt="" />
+        <img src="https://images.unsplash.com/photo-1595506071728-08ef92e2dc72?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="box" alt="" />
       </Link>
       <Link to="/Vodka" className="image-gallery-with-text-overlay">
       <div className="text-with-text-overlay">VODKA</div>
-        <img src="/src/assets/fotos-galeria-categorias/vodka.avif" className="box" alt="" />
+        <img src="https://images.unsplash.com/photo-1595389052748-7b353892845e?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="box" alt="" />
       </Link>
       <Link to="/Whisky" className="image-gallery-with-text-overlay" >
       <div className="text-with-text-overlay">WHISKY</div>
-        <img src="/src/assets/fotos-galeria-categorias/whisky.avif" className="box" alt="" />
+        <img src="https://www.goalongliquor.com/upload/news/1658211568314979.jpg" className="box" alt="" />
       </Link>
       
       <Link to="/Otros" className="image-gallery-with-text-overlay" >
       <div className="text-with-text-overlay">SIN ALCOHOL</div>
-        <img src="/src/assets/fotos-galeria-categorias/bebidas.png" className="box" alt="" />
+        <img src="https://i0.wp.com/www.aquasushi.cl/wp-content/uploads/aqua-botellas-coca.jpg?fit=600%2C600&ssl=1" className="box" alt="" />
       </Link>
       <Link to="/Otros" className="image-gallery-with-text-overlay">
       <div className="text-with-text-overlay">OTROS</div>
-        <img src="/src/assets/fotos-galeria-categorias/bebidas-energeticas.jpg" className="box" alt="" />
+        <img src="https://images.ecestaticos.com/JcgUuZDKIddYQyYqe54_xBvg3lA=/0x0:2272x1425/557x418/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fa9f%2F385%2Ffe3%2Fa9f385fe37c6badcc8a0f81629ae1f98.jpg" className="box" alt="" />
       </Link>
       <Link to="/Ofertas" className="image-gallery-with-text-overlay" >
       <div className="text-with-text-overlay">OFERTAS</div>
-      <img src="/src/assets/fotos-galeria-categorias/ofertas.jpg" className="box" alt="" />
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVAYYd_HjR-knZmNWTNXJCAKi_pmBb3h6DNeTP3kT_Y6g7aX501vCNFADYCmJmYxwkHEc&usqp=CAU" className="box" alt="" />
       </Link>
 
 
