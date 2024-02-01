73425c33db208dd5a327e2471bd43eb53bbe9ec0
Andrés Muñoz <andresestebanmu@gmail.com>
2024-02-01 13:44:51 -0300

    probando imagenes web

--- src/components/GaleriaCategorias.jsx
+++ src/components/GaleriaCategorias.jsx
@@ -9,7 +9,7 @@ export const GaleriaCategorias = () => {
 
       <Link to="/cervezas" className="image-gallery-with-text-overlay" >
       <div className="text-with-text-overlay">CERVEZAS</div>
-        <img src="../assets/fotos-galeria-categorias/cerveza.avif" className="box" type="text" alt="" />
+        <img src="https://images.unsplash.com/photo-1636245364080-8b52c38278a8?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="box" type="text" alt="" />
        
       </Link>
       <Link to="/Espumantes" className="image-gallery-with-text-overlay" >
